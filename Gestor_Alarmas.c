68934ae567ee419f8e47290fd2d1bae09d5c4f1f
Diegol0010 <diegoguiserra@gmail.com>
2021-11-30 19:37:58 +0100

    struct alarmas

--- Gestor_Alarmas.c
+++ Gestor_Alarmas.c
@@ -9,19 +9,15 @@
 
 //Vector cola que sirve para mantener el orden 
 
-static volatile struct evento cola_gestor[8];
+static volatile uint8_t ID[8];//Conocer informacion de los eventos
 static volatile uint8_t periodicas[8];
 static volatile uint8_t activada[8];
-static volatile uint32_t retardos[8];
+static volatile uint32_t retardos[8]; //cuanto le falta a cada una de las alarmas
 
 /************************
 Funcion que inicializa el gestor de alarmas para su posterior funcionamiento.*/
 void gestor_alarmas_init(void){
-	struct evento inicial;
 	for(int i=0;i<8;i++){
-		inicial.auxData=0;
-		inicial.ID_evento=9;
-		cola_gestor[i]=inicial;
 		periodicas[i]=0;
 		activada[i]=0;
 		retardos[i]=0;
@@ -35,20 +31,14 @@ void gestor_alarmas_introducir_evento(uint32_t estructura_alarma){
 	uint8_t ID_evento = estructura_alarma >> 24; //tipo de evento
 	uint8_t esPeriodica=(estructura_alarma >> 23) & 0x01; 
 	uint32_t retardo=estructura_alarma & 0x007FFFFF;	//tiempo evento
-	//Se crea el evento especifico que ha introducido el Set_Alarm
-	struct evento evento_especifico;
-	evento_especifico.ID_evento =ID_evento ;
-	evento_especifico.instante_evento = retardo;
-	//Como necesito volver a usar el retardo en caso de que sea periodica pues 
-	//lo guardo en el propio evento en su campo aux y me evito usar otro vector de almacenaje
-	evento_especifico.auxData=retardo;
-	//Comprobamos si la alarma de ese evento no esta activada 
+	
+	
 	if(activada[ID_evento] == 0){		
-		cola_gestor[ID_evento] = evento_especifico;
+		ID[ID_evento] = ID_evento;
 		periodicas[ID_evento] = esPeriodica;
 		activada[ID_evento] = 1;
 		retardos[ID_evento] = retardo;
-	}
+	} 
 }
 /************************
 Funcion que resta 1 a todos los periodos de los eventos activos*/
@@ -58,12 +48,13 @@ void gestor_alarmas_restar_periodo(void){
 			retardos[i] = retardos[i] - 1;
 			//Si el retardo es 0 pues lo encolas porque ha acabado la alarma
 			if(retardos[i] == 0){
-				cola_guardar_eventos(cola_gestor[i].ID_evento, cola_gestor[i].auxData);
+				//Nos daria igual el valor de AuxData ????
+				cola_guardar_eventos(ID[i], 0);
 				//Ahora al haber acabado se comprueba si es periodica para eliminarlo o no
 				if(periodicas[i] == 0){ //Al no ser periodica se elimina
 					activada[i] = 0;
 				}else{ //En el caso de ser periodica hay que volver a meter el periodo original en el vector
-					retardos[i]=cola_gestor[i].auxData;
+					retardos[i]=retardos[i];
 				}
 			}
 		}
