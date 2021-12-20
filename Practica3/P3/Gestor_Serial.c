5936c51fb0ce5a682065bd08940346788af37599
Diegol0010 <diegoguiserra@gmail.com>
2021-12-20 16:06:23 +0100

    g

--- Practica3/P3/Gestor_Serial.c
+++ Practica3/P3/Gestor_Serial.c
@@ -7,10 +7,13 @@
 #include "Gestor_Serial.h"
 
 
-static int indice_mensaje =0, num_caracteres =0;
+static int indice_mensaje =0, num_caracteres =0;	//indice del caracter y numero de caracteres totales de lo que queremos escribir
 static char* mensaje;
 
-
+/************************************
+Esta función continua escribiendo el mensaje que queremos escribir por la UART, para ello
+preguntamos si el indice_mensaje es menor que el numero de caracteres, si es así le decimos que escriba en indice e
+lo incrementamos una unidad, en caso contrario, pondremos el numero de caracteres a cero y el puntero a mensaje a NULL*/
 void gestor_serial_continuar_mensaje(){
 			if(indice_mensaje<num_caracteres){
 				uart0_sendchar(mensaje[indice_mensaje]);
@@ -22,10 +25,13 @@ void gestor_serial_continuar_mensaje(){
 			}	
 }
 
+/*********************************
+Esta función empeiza a escribir el mensaje que queremos por la UART,primero resetea la variable indice_mensaje, 
+posteriormente comprueba el numero de caracteres y finalmente llama a la función de la uart que escribe.*/
 void gestor_serial_enviar_mensaje(char msg[]){
 			indice_mensaje=0;
 			num_caracteres=strlen(msg);
 			mensaje=msg;
-			uart0_sendchar('\n');			//Preguntar a enrique si se puede invocar sendchar desde aqui
+			uart0_sendchar('\n');		
 }
 
